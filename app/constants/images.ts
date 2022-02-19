e94eab371df61504ddce246bd8800fb9077318f4
gixxerblade <sdclarkie@gmail.com>
2022-02-19 13:17:38 -0500

    Add files

---
+++ app/constants/images.ts
@@ -0,0 +1,5 @@
+export const images = [
+  "https://res.cloudinary.com/gixxerblade/image/upload/v1645284615/gb/bike2_lhhwa0.jpg",
+  "https://res.cloudinary.com/gixxerblade/image/upload/v1645284614/gb/stunt2_mc5zx2.jpg",
+  "https://res.cloudinary.com/gixxerblade/image/upload/v1645284614/gb/skateboards1_o3riiw.jpg",
+];
